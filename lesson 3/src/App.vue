45c090b1d3c5cede3c6e954748a5139577c3a428
Anna Vakula <annavakula7@gmail.com>
2023-05-25 15:53:47 +0300

    New project

---
+++ lesson 3/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="price-block">
+    <p>Original Price: {{ originalPrice }}</p>
+    <p>Discount: {{ discount }}</p>
+    <p>Final Price: {{ finalPrice }}</p>
+  </div>
+</template>
+
+<script>
+import { defineComponent, ref, computed } from "vue";
+
+export default defineComponent({
+  name: "App",
+  components: {},
+  setup() {
+
+    const originalPrice = ref(100)
+    const discount = ref(20)
+
+    const finalPrice = computed(() => {
+      return originalPrice.value - discount.value;
+    })
+
+    return {
+      originalPrice,
+      discount,
+      finalPrice
+    }
+  }
+})
+</script>
+
+<style>
+
+</style>
